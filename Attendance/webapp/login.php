eeb1d9411d5c1e179d4c3d0fd1bf1d07afdf29dc
ngkerong <55265526+ngkerong@users.noreply.github.com>
2023-03-12 00:53:05 +0800

    Update login.php

--- Attendance/webapp/login.php
+++ Attendance/webapp/login.php
@@ -5,21 +5,37 @@
         $password = sha1($_POST["password"]);
         $stmt = $conn->prepare("SELECT * FROM register_user WHERE email = '$email' AND password = '$password'");
         $stmt->execute();
-        $number_of_rows = $stmt->fetchColumn();
+        $number_of_rows = $stmt->fetch(PDO::FETCH_ASSOC);
+        $updatelogin = "UPDATE register_user SET lastlogin = now()";
 
         if($number_of_rows > 0){
+            session_start();
+            $_SESSION["sessionid"] = session_id();
+            $_SESSION["email"] = $number_of_rows['email'];
+            $_SESSION["password"] = $number_of_rows['password'];
+            $_SESSION["type"] = $number_of_rows['type'];
+            $conn->exec($updatelogin);
             echo "<script>alert('Login Success'); </script>";
-            echo "<script> window.location.replace('index.html')</script>";
+            echo "<script> window.location.replace('index.php')</script>";
         }else {
             echo "<script>alert('Login Failure');</script>";
-            echo "<script> window.location.replace('login.php')</script>";
+        }
+    }
+
+    if (isset($_GET["status"])) {
+        if (($_GET["status"] == "logout")) {
+            session_start();
+            session_unset();
+            session_destroy();
+            echo "<script> alert('Session Cleared')</script>";
         }
     }
     
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
+    <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,10 +44,11 @@
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="style/style.css">
+        <script src="function/script.js"></script>
         <title>Login</title>
     </head>
 
-    <body>
+    <body onload = loadCookies();>
         <div class="w3-header w3-container w3-grey w3-padding-20 w3-center">
                 <h1 style="font-size:calc(8px + 4vw);">Attendance</h1>
                 <P style="font-size:calc(8px + 1vw);;">Everyday is AWESOME</P>
@@ -42,6 +59,7 @@
                 <div class="w3-container w3-blue-grey">
                     <h2>Login</h2>
                 </div>
+
                 <form name="loginForm" class="w3-container" action="login.php" method="post">
                     <p>
                         <label class="w3-text-black"><b>Useremail</b></label>
@@ -50,11 +68,11 @@
 
                     <p>
                         <label class="w3-text-black"><b>Password</b></label>
-                        <input class="w3-input w3-border w3-round" type="password" name="password" id="idpassword" required>
+                        <input class="w3-input w3-border w3-round" type="password" name="password" id="idpass" required>
                     </p>
                     
                     <p>
-                        <input class="w3-check" type="checkbox" id="idremember">
+                        <input class="w3-check" type="checkbox" id="idremember" onclick="rememberMe()" name = "remember">
                         <label>Remember me</label>
                     </p>
 
@@ -64,7 +82,6 @@
                 </form> 
             </div>    
         </div>
-
         <footer class="w3-container w3-center w3-blue-grey">
             <p>Copyright by OTC</p>
         </footer>
